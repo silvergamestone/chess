392c129893649a9d03924784136c2b7d6cc58a95
silvergamestone <silvergamestone@gmail.com>
2016-11-12 17:07:22 -0800

    Initinal executable code.

---
+++ src/com/silver/game/Chess.java
@@ -0,0 +1,12 @@
+package com.silver.game;
+
+//Default main class
+public class Chess {
+
+	
+	public static void main(String[] args) {
+		//Basic hello world test
+		System.out.println("Hello World, welcome to chess!");
+	}
+
+}
