3652e60f7e1e922531c683a87b74ab1888c9add7
silvergamestone <silvergamestone@gmail.com>
2016-11-13 16:34:26 -0800

    Added message area and chat button.

--- src/com/silver/game/gui/ChatGUI.java
+++ src/com/silver/game/gui/ChatGUI.java
@@ -1,12 +1,16 @@
 package com.silver.game.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class ChatGUI extends JFrame{
+public class ChatGUI extends JFrame implements ActionListener{
 	
 	/**
 	 * Auto generated serialized ID
@@ -19,19 +23,46 @@ public class ChatGUI extends JFrame{
 	private String title = "Chat GUI";
 	
 	private JTextArea chatArea;
+	private JTextArea messageArea;
+	
+	private JButton chatButton;
 	
 	public ChatGUI(){
 		super();
 		this.setTitle(title);
 		
+		//Create the South Panel
+		JPanel southPanel = new JPanel(new BorderLayout());
+		
+		//Chat Area for typing messages
 		chatArea = new JTextArea();
 		chatArea.setRows(chatRowSize);
 		chatArea.setLineWrap(true);
 		JScrollPane chatScrollPane = new JScrollPane(chatArea);
-		this.add(chatScrollPane, BorderLayout.SOUTH);
+		//this.add(chatScrollPane, BorderLayout.SOUTH);
+		southPanel.add(chatScrollPane);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(frameWidth, frameHeight);
-		setVisible(true);
+		chatButton = new JButton("Send");
+		chatButton.addActionListener(this);
+		
+		southPanel.add(chatButton, BorderLayout.EAST);
+		this.add(southPanel, BorderLayout.SOUTH);
+		
+		//Message Area - contains all previous messages
+		messageArea = new JTextArea();
+		messageArea.setEditable(false);
+		JScrollPane messageScrollPane = new JScrollPane(messageArea);
+		this.add(messageScrollPane);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(frameWidth, frameHeight);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String chatText = chatArea.getText();
+		chatArea.setText("");
+		messageArea.append(chatText+System.lineSeparator());
 	}
 }
